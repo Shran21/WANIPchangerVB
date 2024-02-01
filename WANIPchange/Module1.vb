0b9bec09b291ced8d72dc89fd98491b3a6c5569b
Shran <bsgoszjanos@gmail.com>
2024-02-01 23:36:26 +0100

    WANIP

---
+++ WANIPchange/Module1.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Net
+Module Module1
+    Function GetIP() As String
+        Dim IP As New WebClient
+        Return IP.DownloadString("http://icanhazip.com/")
+    End Function
+    Sub Main()
+        Dim fájlba As New System.IO.StreamWriter("C:\ddupdate")
+        fájlba.WriteLine("server robolution.eu" & vbNewLine & "zone poligon-it.hu" & vbNewLine & "update delete polisys.poligon-it.hu. A" & vbNewLine & "update add polisys.poligon-it.hu. 300 A " & GetIP() & "show" & vbNewLine & "send")
+        fájlba.Close()
+        Process.Start("C:\ipupdate.bat")
+    End Sub
+End Module
